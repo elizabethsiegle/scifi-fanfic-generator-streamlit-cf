9be193aca820db3513ef4ac412588631c3047409
Lizzie Siegle <esiegle@brynmawr.edu>
2024-05-06 16:09:32 -0700

    mothers day

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ load_dotenv()
 CLOUDFLARE_ACCOUNT_ID = os.environ.get("CF_ACCOUNT_ID")
 CLOUDFLARE_API_TOKEN= os.environ.get("CF_API_TOKEN")
 
-def gen_story(model, q1, q2, q3, q4, q5, sal_met):
-    prompt = f"Return only a humorous science-fiction story somehow relating to {q1}, {q2}, {q3}, {q4}, {q5}. Return nothing else. The story should be rated {sal_met} have an introduction paragraph, a villain and conflict, and a conclusion paragraph"
+def gen_poem(model, name, q2, q3, q4, q5):
+    prompt = f"Return only a poem for mother's day for {name} somehow relating to {q2}, {q3}, {q4}, {q5}. Return nothing else."
     print(f'prompt {prompt}')
     payload = {
         "max_tokens": 2000,
@@ -40,28 +40,24 @@ def main():
             }
         </style>
     """, unsafe_allow_html=True)
-    st.markdown('<p class="big-font"<p>Scifi fanfic generator⭐️🔫📝🤖</p>', unsafe_allow_html=True)
-    st.write(":blue[This Python🐍 web🕸️ app is built👩🏻‍💻 w/ [Streamlit](https://streamlit.io/), [Cloudflare Workers AI](https://ai.cloudflare.com/)]")
-    q1 = st.text_input(":red[Who is your favorite Star Wars⭐️🔫 character?]")
+    st.markdown('<p class="big-font"<p>Mother\'s Day Poem and Gift Generator🎁💐</p>', unsafe_allow_html=True)
+    st.write(":blue[This Python🐍 web🕸️ app is built👩🏻‍💻 w/ [Streamlit](https://streamlit.io/) && [Cloudflare Workers AI](https://ai.cloudflare.com/)]")
+    name = st.text_input(":red[What is your mom\'s name?]")
     q2 = st.multiselect(
-        ':green[Your ideal vacation spot is]',
-        ['grasslands🦁', 'mountains⛰️', 'tropical🏝️', 'jungle🌲', 'rainforests💧', 'cityscape🌆', 'caves', 'lakes🚤', 'ice canyons', 'urban🏢', 'swamp👹', 'reefs🐟', 'plains', 'volcanoes🌋', 'arid🌵', 'tundra🥶'],
-        ['ice canyons', 'swamp👹', 'cityscape🌆'])
+        ':green[Your mom\'s ideal vacation spot is]',
+        ['Hawaii🏝️', 'Tahoe', 'Paris', 'Tokyo'],
+        ['Hawaii🏝️'])
     q2 = str(q2)
     st.markdown("![do it meme](https://media1.giphy.com/media/3o84sw9CmwYpAnRRni/giphy.gif)")
-    char_labels = ["Jabba the Hutt", "Admiral Ackbar", "Jar Jar Binks", "Count Dooku", "Obi-wan Kenobi", "Poe Dameron"]
-    q3 = st.select_slider(':orange[On a scale from Jabba the Hutt to Poe Dameron, how 🔥 is the villain✈️ of your fanfic?]', options=char_labels)
+    warm_labels = ["popcorn", "piping hot ramen", "Anakin burning on Geonosis"]
+    q3 = st.select_slider(':orange[On a scale from popcorn to Anakin burning on Geonosis, how warm🔥 does your mom make you feel?]', options=warm_labels)
 
-    q4 = st.text_input(":yellow[Describe a starship⭐️🚀 to drive🚗]")
-    q5 = st.text_input(":pink[Describe u✌️🥰]")
-    sal_labels = ["R2D2--like the innocent astromech droid, suitable for all", "Chewbacca moaning--could go over young heads", "so uncivilized! May be inappropriate for species < 13", "Jar Jar || Jabba sans-clothes--may contain content !suitable for < 17 w/o parental consent"]
-    salaciousness_met = st.select_slider(":orange[On a scale from R2-D2😇 to Jar Jar sans-clothes🔥, how salacious🥵 should the fanfic be😘?]", options= sal_labels)
-
-    st.markdown("![screaming r2d2 gif](https://assets.teenvogue.com/photos/572a3302321c4faf6ae8a317/16:9/w_2580,c_limit/R2SCREAM.gif)") #screaming r2
-
+    q4 = st.text_input(":pink[Describe your mom✌️🥰]")
+    sal_labels = ["har gow", "shiu mai", "potsticker🥟", ]
+    q5 = st.select_slider(":orange[On a scale from R2-D2😇 to Jar Jar sans-clothes🔥, how cute should the poem be😘?]", options= sal_labels)
     # All models at https://developers.cloudflare.com/workers-ai/models/
     img_model = st.selectbox(
-    "Choose your character (Text-To-Image model):",
+    "Choose your character😘 (Text-To-Image model):",
         options=(
             "@cf/lykon/dreamshaper-8-lcm",
             "@cf/bytedance/stable-diffusion-xl-lightning",
@@ -69,9 +65,9 @@ def main():
         ),
     )
     text_model = st.selectbox(
-        "Choose your weapon (Text generation model):",
+        "Choose your flower🌸 (Text generation model):",
         options= (
-            # "@cf/meta/llama-2-7b-chat-fp16",
+            "@cf/meta/llama-2-7b-chat-fp16",
             "@cf/meta/llama-2-7b-chat-int8",
             "@cf/meta/llama-3-8b-instruct",
             "@cf/mistral/mistral-7b-instruct-v0.2-lora"
@@ -79,12 +75,10 @@ def main():
     )
     # url =f"https://api.cloudflare.com/client/v4/accounts/{CLOUDFLARE_ACCOUNT_ID}/ai/run/{text_model}"
     url =f"https://api.cloudflare.com/client/v4/accounts/{CLOUDFLARE_ACCOUNT_ID}/ai/run/{text_model}"
-    if q1 is not None and q2 is not None and q3 is not None and q4 is not None and q5 is not None and salaciousness_met is not None and st.button('Generate🤖'):
+    if name is not None and q2 is not None and q3 is not None and q4 is not None and st.button('Generate🤖'):
         # load dataset once on page load/on server start
         with st.spinner('Processing📈...'):
-            st.markdown("![i will finish what you started gif](https://y.yarn.co/2f4fabe4-6046-4bbd-96bd-3a3ccf9853c9_text.gif)")
-            
-            img_prompt = f"You are a world-renowned painter of lighthearted sci-fi art. Generate a humorous image relating to {q1} and {q2}"
+            img_prompt = f"You are a world-renowned painter of matronly art. Generate a cute, airy, light image without any people in it relating to {q4} and {q2}"
             #img_prompt = f"Generate a seductive image of Jar Jar Binks"
             img_url =f"https://api.cloudflare.com/client/v4/accounts/{CLOUDFLARE_ACCOUNT_ID}/ai/run/{img_model}"
             headers = {
@@ -96,7 +90,7 @@ def main():
                 json={"prompt": img_prompt},
             )
             st.image(resp.content, caption=f"AI-generated image from {img_model}") #bytes lmao
-            story = gen_story(text_model, q1, q2, q3, q4, q5, salaciousness_met)['result']['response']
+            story = gen_poem(text_model, name, q2, q3, q4, q5)['result']['response']
             
             html_str = f"""
             <p style="font-family:Comic Sans; color:Pink; font-size: 18px;">{story}</p>
